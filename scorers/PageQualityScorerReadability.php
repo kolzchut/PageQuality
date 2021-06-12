d8c30a9942bc957bc8864989ea6cadb9199cbedd
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2021-06-12 22:45:38 +0300

    Rudimentary fix for PageQualityScorerReadability to work for Hebrew

--- scorers/PageQualityScorerReadability.php
+++ scorers/PageQualityScorerReadability.php
@@ -21,15 +21,35 @@ class PageQualityScorerReadability{
 		return self::$checksList;
 	}
 
+	/***
+	 * @link https://www.php.net/manual/en/function.str-word-count.php#107363
+	 *
+	 * This simple utf-8 word count function (it only counts)
+	 * is a bit faster then the one with preg_match_all
+	 * about 10x slower then the built-in str_word_count
+	 *
+	 * If you need the hyphen or other code points as word-characters
+	 * just put them into the [brackets] like [^\p{L}\p{N}\'\-]
+	 * If the pattern contains utf-8, utf8_encode() the pattern,
+	 * as it is expected to be valid utf-8 (using the u modifier).
+	 **/
+
+	// Jonny 5's simple word splitter
+	function str_word_count_utf8($str) {
+		return count(preg_split('~[^\p{L}\p{N}\']+~u',$str));
+	}
+
 	public function calculatePageScore( $text ) {
 		$response = [];
 
-		$dom = new DOMDocument();
-		$dom->loadHTML( $text );
+		// @todo load only actual page content. right now this will also load stuff like the "protectedpagewarning" message
+		$dom = new DOMDocument('1.0', 'utf-8');
+		// Unicode-compatibility - see https://stackoverflow.com/questions/8218230/php-domdocument-loadhtml-not-encoding-utf-8-correctly
+		$dom->loadHTML( '<?xml encoding="utf-8" ?>' . $text );
 		$pNodes = $dom->getElementsByTagName('p');
 
 		foreach ( $pNodes as $pNode ) {
-			$wc = str_word_count( $pNode->nodeValue );
+			$wc = self::str_word_count_utf8( $pNode->nodeValue );
 
 			$score = "green";
 			if ( $wc >= self::$checksList["para_length"][PageQualityScorer::RED] ) {
@@ -47,7 +67,7 @@ class PageQualityScorerReadability{
 
 			$sentences = preg_split('/(?<=[.?!])\s+(?=[a-z])/i', $pNode->nodeValue);
 			foreach( $sentences as $sentence ) {
-				$wc = str_word_count( $sentence );
+				$wc = self::str_word_count_utf8( $sentence );
 
 				$score = "green";
 				if ( $wc >= self::$checksList["sentence_length"][PageQualityScorer::RED] ) {
