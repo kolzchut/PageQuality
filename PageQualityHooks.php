5f259eb7fa426e6ca3f8ec9545ad9c433850f849
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2021-10-05 16:45:47 +0300

    Lint the Hooks class

--- PageQualityHooks.php
+++ PageQualityHooks.php
@@ -5,22 +5,18 @@ use MediaWiki\MediaWikiServices;
 class PageQualityHooks {
 
 	public static function onPageSaveComplete( WikiPage $wikiPage, MediaWiki\User\UserIdentity $user, string $summary, int $flags, MediaWiki\Revision\RevisionRecord $revisionRecord, MediaWiki\Storage\EditResult $editResult ) {
-		global $wgOut, $wgTitle;
-
-		if ( $wgTitle->getNamespace() == 0 && PageQualityScorer::isPageScoreable( $wgOut->getTitle() ) ) {
-			list( $score, $responses ) = PageQualityScorer::runScorerForPage( $wgOut->getTitle(), $wgOut->getHTML() );
+		if ( $wikiPage->getTitle()->getNamespace() === NS_MAIN && PageQualityScorer::isPageScoreable( $wikiPage->getTitle() ) ) {
+			list( $score, $responses ) = PageQualityScorer::runScorerForPage( $wikiPage->getTitle() );
 		}
 	}
 
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
-		global $wgOut, $wgTitle;
+		if ( $out->getTitle()->getNamespace() === NS_MAIN && PageQualityScorer::isPageScoreable( $out->getTitle() ) ) {
+			list( $score, $responses ) = PageQualityScorer::getScorForPage( $out->getTitle() );
 
-		if ( $wgTitle->getNamespace() == 0 && PageQualityScorer::isPageScoreable( $wgOut->getTitle() ) ) {
-			list( $score, $responses ) = PageQualityScorer::getScorForPage( $wgOut->getTitle() );
-
-			$wgOut->setIndicators( [
+			$out->setIndicators( [
 				"pq_status" =>
-					'<a class="page_quality_show" data-page_id="'.$wgOut->getTitle()->getArticleID().'">' . $out->msg( 'pq_quality_score_link' )->escaped() . ' <span class="badge">'. $score .'</span></a>
+					'<a class="page_quality_show" data-page_id="'.$out->getTitle()->getArticleID().'">' . $out->msg( 'pq_quality_score_link' )->escaped() . ' <span class="badge">'. $score .'</span></a>
 					'
 			]);
 			$out->addModules( 'ext.page_quality' );
