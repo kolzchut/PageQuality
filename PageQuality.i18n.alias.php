36e993e39b67fa06c51d9cc1c61a31d093fd4ea3
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2021-11-17 19:23:55 +0200

    Add an i18n alias file for the special page

---
+++ PageQuality.i18n.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Aliases for Special:PageQuality
+ */
+
+$specialPageAliases = [];
+
+/** English
+ */
+$specialPageAliases['en'] = [
+	'PageQuality' => [ 'PageQuality', 'Page Quality' ],
+];
