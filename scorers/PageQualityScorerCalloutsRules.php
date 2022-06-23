7d721eead0f03672d3b20e2e1f8012ced49f6b89
Nischay Nahata <nischayn22@gmail.com>
2022-06-23 10:14:22 +0530

    Callouts should not be next to each other

--- scorers/PageQualityScorerCalloutsRules.php
+++ scorers/PageQualityScorerCalloutsRules.php
@@ -3,12 +3,12 @@
 class PageQualityScorerCalloutsRules extends PageQualityScorer{
 
 	public static $checksList = [
-		// "callout_gaps" => [
-		// 	"name" => "pag_scorer_callout_gaps",
-		// 	"description" => "callout_gaps_desc",
-		// 	"check_type" => "exist",
-		// 	"severity" => PageQualityScorer::YELLOW,
-		// ],
+		"callout_gaps" => [
+			"name" => "pag_scorer_callout_gaps",
+			"description" => "callout_gaps_desc",
+			"check_type" => "exist",
+			"severity" => PageQualityScorer::YELLOW,
+		],
 		"callout_section_begin" => [
 			"name" => "pag_scorer_callout_section_begin",
 			"description" => "callout_section_begin_desc",
@@ -44,6 +44,18 @@ class PageQualityScorerCalloutsRules extends PageQualityScorer{
 	        	if ( in_array( $previousNode->tagName, [ "h1", "h2", "h3", "h4" ] ) ) {
 					$response['callout_section_begin'][] = [
 						"score" => self::getCheckList()['callout_section_begin']['severity'],
+						"example" => $divNodes->item($i)->textContent
+					];
+	        	}
+	        	$class = $previousNode->getAttribute('class');
+		        if ( 
+		        	stripos($class, "wr-example") !== false || 
+		        	stripos($class, "wr-tip") !== false || 
+		        	stripos($class, "wr-please-note") !== false || 
+		        	stripos($class, "wr-warning") !== false 
+		       	) {
+					$response['callout_gaps'][] = [
+						"score" => self::getCheckList()['callout_gaps']['severity'],
 						"example" => $divNodes->item($i)->textContent
 					];
 	        	}
